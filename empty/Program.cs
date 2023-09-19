955e0dc670de962481531ed50cdd8adab4bd754c
asabdulkareem <as.abdulkareem@gmail.com>
2023-09-19 14:27:24 +0530

    Added middle wares

--- empty/Program.cs
+++ empty/Program.cs
@@ -1,5 +1,12 @@
 // Create
-var builder = WebApplication.CreateBuilder(args);
+//Step1: Creating an Instance of WebApplicationOptions Class
+WebApplicationOptions webApplicationOptions = new WebApplicationOptions
+{
+    WebRootPath = "MyWebRoot", //Setting the WebRootPath as MyWebRoot
+    Args = args, //Setting the Command Line Arguments in Args
+    EnvironmentName = "Production", //Changing to Production
+};
+var builder = WebApplication.CreateBuilder(webApplicationOptions);
 // Set up Web Server
 // Host the Application
 // Logging
@@ -26,15 +33,34 @@ string? MyCustomKeyValueDirect = configuration["MyCustomKey"];
 // The Build method of the WebApplicationBuilder class creates a new instance of the WebApplication class. We then use the Web Application instance to set up the Middlewares and endpoints for our ASP.NET Core Application. The template has set one middleware component using the MapGet extension method as follows:
 //app.MapGet("/", () => $"Hello World!\n{System.Diagnostics.Process.GetCurrentProcess().ProcessName}\n{MyCustomKeyValue}\n{MyCustomKeyValueDirect}");
 //Run (Starts the Application) - The Run method of the WebApplication instance starts the application and listens to HTTP requests.
-//Configuring New Inline Middleware Component using Run Extension Method
-app.Use(async (context, next) =>
-{
-    await context.Response.WriteAsync("Getting Response from First Middleware");
-    await next();
-});
-//Configuring New Middleware Component using Run Extension Method
-app.Run(async (context) =>
-{
-    await context.Response.WriteAsync("Getting Response from Second Middleware");
-});
+//Configuring Middleware Component using Use and Run Extension Method
+//First Middleware Component Registered using Use Extension Method
+//app.Use(async (context, next) =>
+//{
+//    await context.Response.WriteAsync("Middleware1: Incoming Request\n");
+//    //Calling the Next Middleware Component
+//    await next();
+//    await context.Response.WriteAsync("Middleware1: Outgoing Response\n");
+//});
+////Second Middleware Component Registered using Use Extension Method
+//app.Use(async (context, next) =>
+//{
+//    await context.Response.WriteAsync("Middleware2: Incoming Request\n");
+//    //Calling the Next Middleware Component
+//    await next();
+//    await context.Response.WriteAsync("Middleware2: Outgoing Response\n");
+//});
+////Third Middleware Component Registered using Run Extension Method
+//app.Run(async (context) =>
+//{
+//    await context.Response.WriteAsync("Middleware3: Incoming Request handled and response generated\n");
+//    //Terminal Middleware Component i.e. cannot call the Next Component
+//});
+app.MapGet("/", () => $"EnvironmentName: {app.Environment.EnvironmentName} \n" +
+            $"ApplicationName: {app.Environment.ApplicationName} \n" +
+            $"WebRootPath: {app.Environment.WebRootPath} \n" +
+            $"ContentRootPath: {app.Environment.ContentRootPath}");
+            //This will Run the Application
+            app.Run();
+//This will Start the Application
 app.Run();
